29d5b71800b8e2a59c73966a964d7b65be250bc2
Mykhaylo Kozachok <Mykhaylo_Kozachok@epam.com>
2019-11-21 00:03:10 +0200

    Add get by id api

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var db = require('./db')
 app.use(cors())
 
 app.get('/products', function (req, res) {
-    res.status(200).send({items: db})
+    res.status(200).send({data: db.db}) // FIXME db.db
+});
+
+app.get('/products/:id', function (req, res) {
+    var id = req.params.id
+    res.status(200).send({data: db.db.find(function(item) {
+        return item.id == id
+    })})
 });
 
 app.listen(process.env.PORT || 5000)
