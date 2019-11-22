29768a674aed2a1239c6a4fc745c6da692431b91
Mykhaylo Kozachok <Mykhaylo_Kozachok@epam.com>
2019-11-22 23:25:55 +0200

    Updated api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var uuidv1 = require('uuid/v1');
-var mongoose = require('mongoose');
-import 'dotenv/config';
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var uuidv1 = require("uuid/v1");
+var mongoose = require("mongoose");
+import "dotenv/config";
 var app = express();
-var db = require('./db');
+var db = require("./db");
 
 // API
-var productApi = require('./src/api/product/controller')
+import { productApi } from "./src/api/product/controller";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,91 +24,114 @@ mongoose.connect(process.env.DB_CONNECT);
 
 // Products API
 // GET
-app.get('/products', function (req, res) {
-    res.status(200).send({data: db.db}); // FIXME db.db
+app.get("/products", function(req, res) {
+  productApi.getAll(data => {
+    res.status(200).send({ data });
+  });
 });
 
-app.get('/products/:id', function (req, res) {
-    var id = req.params.id;
-    var product = db.db.find(function(item) {
-        return item.id == id;
-    })
+app.get("/products/:id", function(req, res) {
+  var id = req.params.id;
+  // var product = db.db.find(function(item) {
+  //     return item.id == id;
+  // })
 
+  // if (product) {
+  //     res.status(200).send({data: product})
+  // }
+
+  productApi.get(id, product => {
     if (product) {
-        res.status(200).send({data: product})
+      return res.status(200).send({ data: product });
     }
-
     return res.status(404).send({
-        success: 'false',
-        message: 'product not found',
-      });
+      success: "false",
+      message: "product not found"
+    });
+  });
 });
 
 // POST
-app.post('/products', function (req, res) {
-    if(!req.body.title) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'title is required'
-        });
-      } else if(!req.body.description) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'description is required'
-        });
-      } else if(!req.body.price) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'price is required'
-        });
-      }
+app.post("/products", function(req, res) {
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required"
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required"
+    });
+  } else if (!req.body.price) {
+    return res.status(400).send({
+      success: "false",
+      message: "price is required"
+    });
+  }
 
-      // var product = {
-      //   id: uuidv1(),
-      //   title: req.body.title,
-      //   description: req.body.description,
-      //   previewImage: req.body.previewImage | '',
-      //   price: req.body.price,
-      // }
+  // var product = {
+  //   id: uuidv1(),
+  //   title: req.body.title,
+  //   description: req.body.description,
+  //   previewImage: req.body.previewImage | '',
+  //   price: req.body.price,
+  // }
 
-      // db.db.push(product);
+  // db.db.push(product);
 
-      productApi.create(req.body, (newProduct) => {
-        res.status(201).send({
-          success: 'true',
-          message: 'product added successfully',
-          data: newProduct
-        })
-      })
+  productApi.create(req.body, newProduct => {
+    res.status(201).send({
+      success: "true",
+      message: "product added successfully",
+      data: newProduct
+    });
+  });
 
-      // return res.status(201).send({
-      //   success: 'true',
-      //   message: 'product added successfully',
-      //   data: product
-      // })
+  // return res.status(201).send({
+  //   success: 'true',
+  //   message: 'product added successfully',
+  //   data: product
+  // })
+});
+
+// PUT
+app.put("/products", function(req, res) {
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "title is required"
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "description is required"
+    });
+  } else if (!req.body.price) {
+    return res.status(400).send({
+      success: "false",
+      message: "price is required"
+    });
+  }
+
+  productApi.update(req.body, updatedProduct => {
+    res.status(201).send({
+      success: "true",
+      message: "product updated successfully",
+      data: updatedProduct
+    });
+  });
 });
 
 // DELETE
-app.delete('/products/:id', function (req, res) {
-    var id = req.params.id;
-    var product = db.db.find(function(item) {
-        return item.id == id;
-    })
-
-    if (product) {
-        db.db = db.db.filter(function(item) {
-            return item.id !== id
-        })
-        return res.status(200).send({
-            success: 'true',
-            message: 'product deleted successfuly',
-          });
-    }
-
-    return res.status(404).send({
-        success: 'false',
-        message: 'product not found',
-      });
+app.delete("/products/:id", function(req, res) {
+  var id = req.params.id;
+  productApi.remove(id, () => {
+    return res.status(200).send({
+      success: "true",
+      message: "product deleted successfully"
+    });
+  });
 });
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
--- src/api/product/controller.js
+++ src/api/product/controller.js
@@ -11,6 +11,47 @@ const create = function(data, callback) {
   });
 };
 
+const get = function(id, callback) {
+  Product.findById(id, (err, product) => {
+    if (err) throw err;
+    callback(product);
+  });
+};
+
+const getAll = function(callback) {
+  Product.find({}, (err, product) => {
+    if (err) throw err;
+    callback(product);
+  });
+};
+
+const update = function(data, callback) {
+  Product.findById(data._id, (err, product) => {
+    if (err) throw err;
+
+    product.title = data.title;
+    product.price = data.price;
+    product.description = data.description;
+    product.previewImage = data.previewImage;
+
+    product.save(function(err) {
+      if (err) throw err;
+      callback(product);
+    });
+  });
+};
+
+const remove = function(id, callback) {
+  Product.findByIdAndRemove(id, err => {
+    if (err) throw err;
+    callback();
+  });
+};
+
 export const productApi = {
-  create
+  get,
+  getAll,
+  create,
+  update,
+  remove
 };
