8d1deb18b05d282e1b163b6dbb4d2a4222f699de
Mykhaylo Kozachok <Mykhaylo_Kozachok@epam.com>
2019-12-08 12:42:14 +0200

    Clean

--- src/index.js
+++ src/index.js
@@ -11,16 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env && process.env.ENV === "local2") {
-  mongoose.connect(process.env.DB_CONNECT);
-} else {
-  //Connect to mlab
-  mongoose.connect(
-    // "mongodb+srv://mkozachok:MTeam2019@cluster0-klifg.mongodb.net/test?retryWrites=true&w=majority",
-    "mongodb+srv://admin:ndhFqHe53Er0bHGk@cluster0-xx9qb.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  );
-}
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
 
 app.use(router);
 app.get("/", (req, res) => {
